ae12cd1035b6682049ce76520c5400f1b2d2e619
uni-3 <7pluto8@gmail.com>
2020-10-11 05:02:27 +0900

    add user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,32 @@
 class UsersController < ApplicationController
-  def new
+
+  def show
+    @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      # ok
+      # flash変数に代入したメッセージは、リダイレクトした直後のページで表示できるようになります。
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      # ng
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
+  post '/signup',  to: 'users#create'
+
+  # usersリソースで必要となるurl httpリクエストが有効になる
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
