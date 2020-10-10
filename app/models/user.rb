922a7aafd2b40339e9b4d3f2a8ea796378148521
uni-3 <7pluto8@gmail.com>
2020-10-11 03:57:01 +0900

    add user validation password

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  before_save {email.downcase!}
+
+  validates :name, presence: true, length: {maximum: 50}
+
+  #VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 255},
+                    format: {with: VALID_EMAIL_REGEX},
+                    uniqueness: {case_sensitive: false}
+
+  # セキュアにハッシュ化したパスワードを、データベース内のpassword_digestという属性に保存できるようになる。
+  # 2つのペアの仮想的な属性 (passwordとpassword_confirmation) が使えるようになる。また、存在性と値が一致するかどうかのバリデーションも追加される18 。
+  # authenticateメソッドが使えるようになる (引数の文字列がパスワードと一致するとUserオブジェクトを、間違っているとfalseを返すメソッド) 。
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+
+end
